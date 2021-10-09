b50d4c39864d7d9a980c06c0f8a2612eb320f125
Tuncvrjs <90102420+Tuncvrjs@users.noreply.github.com>
2021-10-09 10:29:50 +0300

    Delete tuncvr-db directory

--- tuncvr-db/main.js
+++
@@ -1,4 +0,0 @@
-exports.set = require("./adapters/set.js")
-exports.add = require("./adapters/add.js")
-exports.deleteAll = require("./adapters/deleteAll.js")
-exports.fetch = require("./adapters/fetch.js")
